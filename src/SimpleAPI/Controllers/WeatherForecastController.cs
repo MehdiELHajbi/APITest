242946b8e7855845e8f9d0b9cfac89d3ad89fbb5
mehdi <elhajbi.mehdi@gmail.com>
2023-08-23 11:25:09 +0200

    update controller

--- src/SimpleAPI/Controllers/WeatherForecastController.cs
+++ src/SimpleAPI/Controllers/WeatherForecastController.cs
@@ -21,7 +21,7 @@ public class WeatherForecastController : ControllerBase
     [HttpGet]
     public ActionResult<IEnumerable<string>> Get()
     {
-        return new string[] { "Mehdi-1", "Mehdi-2" };
+        return new string[] { "Mehdi-1", "Mehdi-3" };
     }
 
     // GET api/values/5
